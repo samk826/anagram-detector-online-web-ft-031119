c571f62b26377e85cbbcda62c0e8579799999b19
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-25 01:28:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,4 +6,10 @@ class Anagram
     @word = word
   end
 
+def match(word_array)
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
 end # Your code goes here!
